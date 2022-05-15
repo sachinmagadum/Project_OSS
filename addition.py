a8723bdb8974ffc0fd1dc296d3401590fb077f3f
sachinmagadum <sachinmagadum5009@gmail.com>
2022-05-15 17:36:57 +0530

    Initial changes

---
+++ addition.py
@@ -0,0 +1,10 @@
+a = 10
+
+b = 20
+
+def add(a,b):
+    c = a + b
+    return c
+
+ans = add(a,b)
+print("addition:",ans)
---
+++ mul.py
@@ -0,0 +1,14 @@
+a = 10
+
+b = 20
+
+def mul(a,b):
+    c = a * b
+    return c
+
+ans = mul(a,b)
+print("Multiplication:",ans)
+
+
+def div(a,b):
+    pass
